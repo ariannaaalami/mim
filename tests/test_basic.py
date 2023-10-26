4c0b38f511633cae8ad491711dc635370214e2a4
ariannaaalami <138022192+ariannaaalami@users.noreply.github.com>
2023-10-26 13:30:47 +0200

    adding tests for jaccard similarity

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,6 +7,13 @@ def test_package_has_version():
     assert mim.__version__ is not None
 
 
+# def jaccard_test():
+#     jaccard_results_df = pd.read_csv("/Users/2021ariannaa/GitHub/mim/tests/jaccard_test_results.csv")
+#     test_adata = ad.read_h5ad("/Users/2021ariannaa/GitHub/mim/tests/test_data.h5ad")
+#     jaccard = mim.metrics.jaccard_similarity()
+#     jaccard_test_results = jaccard(test_adata, 'cell_ID', 'modality', ['ATAC', 'GEX'])
+
+
 @pytest.mark.skip(reason="This decorator should be removed when test passes.")
 def test_example():
     assert 1 == 0  # This test is designed to fail.
